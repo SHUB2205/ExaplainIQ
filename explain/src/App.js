15a6a199a1f3e4563c7cf83e43ca4a6b8d9c78be
Adarsh Kessani <adarshkessani@gmail.com>
2023-10-22 17:34:52 -0400

    M

--- explain/src/App.js
+++ explain/src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import React from "react";
 import Home from "./components/pages/Home";
 import Login from "./Login";
-
+import AboutUs from "./components/AboutUs";
 function App() {
   return (
     <>
@@ -12,9 +12,10 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" exact Component={Home} />
-          <Route path="/login" component={Login} />
+          <Route path="/login" Component={Login} />
         </Routes>
       </Router>
+      <AboutUs />
     </>
   );
 }
