1dcef4244df255e321756b008b2111a2f2aaab7e
SHUB2205 <parasharshubhankar22@gmail.com>
2023-10-22 14:51:24 -0400

    lOGIN

--- explain/src/App.js
+++ explain/src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import React from "react";
 import Home from "./components/pages/Home";
+import Login from "./Login";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" exact Component={Home} />
+          <Route path="/login" component={Login} />
         </Routes>
       </Router>
     </>
