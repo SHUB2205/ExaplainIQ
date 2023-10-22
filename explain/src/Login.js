5320c0186da54010e78c2145c17ffb5fbee70f1c
jodemustafa <111262084+jodemustafa@users.noreply.github.com>
2023-10-22 18:13:51 -0400

    desktop

--- explain/src/Login.js
+++ explain/src/Login.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import {useNavigate} from 'react-router-dom';
+import './login.css';
 
 function Login({ handleLogin }) {
+  const [Name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
@@ -9,23 +11,37 @@ function Login({ handleLogin }) {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    handleLogin({ email, password });
+    handleLogin({ Name,email, password });
 
     navigate.push('/login-success');
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <h2>Login</h2>
-      <label>Email:
-        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-      </label>
-      <label>Password:
-        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-      </label>
-      <button type="submit">Log In</button>
-    </form>
+    <div className="container">
+      <form onSubmit={handleSubmit}>
+        <h2>Login</h2>
+        <div className="input-container">
+          <label>Name:</label>
+          <input type="text" value={Name} onChange={(e) => setName(e.target.value)} />
+        </div>
+        <div className="input-container">
+          <label>Email:</label>
+          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        </div>
+        <div className="input-container">
+          <label>Password:</label>
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        </div>
+        <button type="submit">Log In</button>
+      </form>
+    </div>
   );
 }
 
+
+
 export default Login;
+
+
+
+
